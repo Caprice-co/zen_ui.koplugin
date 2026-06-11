0b776d1c52f921192df14465a51b191adecacb98
Anthony Gress <the.techx.guy@gmail.com>
2026-06-11 11:38:59 -0400

    minify and reduce images

--- config/changelog.lua
+++ config/changelog.lua
@@ -97,11 +97,13 @@ return {
         "Bug fixes and performance improvements",
     },
     ["2.0.0"] = {
-        "This update has breaking changes, settings will not be saved if you go back to v1.x",
+        "IMPORTANT: Update via settings or completely delete old zen_ui.koplugin",
+        "IMPORTANT: This update has breaking changes, settings will not be saved if you go back to v1.x",
         "New customizable home page",
         "Added rotation options in quick settings",
         "Updated how settings are stored",
         "Reorganized/restructured project",
+        "Optimizations reducing app size by 57%",
         "Bug fixes and performance improvements",
     },
 }
